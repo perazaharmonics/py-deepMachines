3179310526774c926f20a3dc07f58b980cb82749
Enrique, MEngEE DSP <141517235+perazaharmonics@users.noreply.github.com>
2024-02-04 12:36:11 -0500

    Debug n.n
    Modified to use the file format from Digital Image Processing, Gonzalez & Woods

--- ssdCNN.py
+++ ssdCNN.py
@@ -9,7 +9,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Add arguments
-parser.add_argument('--im_path', type=str, default='data/1.jpg', help='path to the image')
+parser.add_argument('--im_path', type=str, help='path to the image directory')
 args = parser.parse_args()
 IM_PATH = args.im_path
 
@@ -24,42 +24,43 @@ MODEL_DIR = '/home/perazaharmonics/Python_ML/ssd_mobilenet_v2_fpnlite_320x320_co
 loaded_model = tf.saved_model.load(MODEL_DIR)
 infer = loaded_model.signatures['serving_default']
 
-# now we will read the images in the specified directory path. Here, we are considering only
-# .jpeg images, but you can change it into another format if you want:
+# Check if IM_PATH is a directory
+if not os.path.isdir(IM_PATH):
+    print(f"Error: The path {IM_PATH} is not a directory.")
+    exit(1)
 
-for dirs in os.listdir(IM_PATH):
-    if not dirs.startswith('.'):
-        for im in os.listdir(os.path.join(IM_PATH, dirs)):
-            if im.endswith('.jpeg'):
-                image = read_image(os.path.join(IM_PATH, dirs, im))
-                if image is None:
-                    print('Image is read as None')
-                else:
-                    print('Image name:', im)
+# Process each image in the directory
+for filename in os.listdir(IM_PATH):
+    if filename.endswith('.tif'):  # Check for '.tif' images
+        image_path = os.path.join(IM_PATH, filename)
+        image = read_image(image_path)
+        if image is None:
+            print(f'Image not read: {filename}')
+            continue
+        print('Processing image:', filename)
 
-                # Preprocess the image as required by the model
-                # This usually involves resizing the image and expanding its dimensions
-                input_tensor = tf.convert_to_tensor(image)
-                input_tensor = input_tensor[tf.newaxis,...]
+        # Preprocess the image as required by the model
+        input_tensor = tf.convert_to_tensor(image)
+        input_tensor = input_tensor[tf.newaxis, ...]
 
-                # Run inference
-                detections = infer(input_tensor)
+        # Run inference
+        detections = infer(input_tensor)
 
-                # Extract boxes and scores
-                boxes = detections['detection_boxes'][0].numpy()
-                classes = detections['detection_classes'][0].numpy().astype(np.int32)
-                scores = detections['detection_scores'][0].numpy()
+        # Extract boxes and scores
+        boxes = detections['detection_boxes'][0].numpy()
+        classes = detections['detection_classes'][0].numpy().astype(np.int32)
+        scores = detections['detection_scores'][0].numpy()
 
-                # Visualization of the results of a detection
-                imageHeight, imageWidth, _ = image.shape
-                for i, box in enumerate(boxes):
-                    if scores[i] > 0.5:  # Confidence threshold
-                        ymin, xmin, ymax, xmax = box
-                        (left, right, top, bottom) = (xmin * imageWidth, xmax * imageWidth,
-                                                      ymin * imageHeight, ymax * imageHeight)
-                        cv2.rectangle(image, (int(left), int(top)), (int(right), int(bottom)), (255, 255, 0), thickness=1)
-                        print('Class:', classes[i], 'Confidence:', scores[i])
+        # Visualization of the results of a detection
+        imageHeight, imageWidth, _ = image.shape
+        for i, box in enumerate(boxes):
+            if scores[i] > 0.5:  # Confidence threshold
+                ymin, xmin, ymax, xmax = box
+                (left, right, top, bottom) = (xmin * imageWidth, xmax * imageWidth,
+                                              ymin * imageHeight, ymax * imageHeight)
+                cv2.rectangle(image, (int(left), int(top)), (int(right), int(bottom)), (255, 255, 0), thickness=1)
+                print('Class:', classes[i], 'Confidence:', scores[i])
 
-                plt.figure(figsize=(10, 10))
-                plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-                plt.show()
+        plt.figure(figsize=(10, 10))
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.show()
